4ebd42be30106b826662789f969c0b6714c022b8
Jakub Drzymała <ordenix@otavi.pl>
2022-01-08 01:34:26 +0100

    add postal_code search by city and fix tariff data

--- src/views/Postal_code.vue
+++ src/views/Postal_code.vue
@@ -2,9 +2,9 @@
   <div class="main">
     <div class="main_container">
       <div id="alert">{{alert_text}}</div>
-      <div class="title">Wprowadź kod:</div>
+      <div class="title">Wprowadź kod lub miasto:</div>
       <input id="postal_input" @click="onInputPostalClick" type="text" name="postal-code" placeholder="Wprowadź kod pocztowy" v-model="postal">
-      <input v-if="false" @click="onInputCityClick" type="text" name="city" placeholder="Wprowadź miasto" v-model="city">
+      <input id="postal_input2" @click="onInputCityClick" type="text" name="city" placeholder="Wprowadź miasto" v-model="city">
       <div class="place_button">
         <div class="orange_button" @click="onButtonClick">
           <div class="button_label">
@@ -15,6 +15,20 @@
       </div>
       <div class="response_place">
         <div id="response">
+          <div v-for="(element, index) in filter_city_data" :key="index">
+            <div v-if="!element.nazwa" id="response_card2">
+              <div>Miejscowość:</div>
+              <div> {{element.miejscowosc}}</div>
+              <div>Gmina:</div>
+              <div>{{element.gmina}}</div>
+              <div>Powiat:</div>
+              <div>{{element.powiat}}</div>
+              <div>Województwo:</div>
+              <div>{{element.wojewodztwo}}</div>
+              <div>Kod pocztowy:</div>
+              <div>{{element.kod}}</div>
+            </div>
+          </div>
           <div v-for="( element, index) in array_data" :key="index">
             <div v-if="!element.nazwa" id="response_card">
               <div>Miejscowość:</div>
@@ -36,18 +50,31 @@
 <script>
 import axios from 'axios'
 export default {
+  computed: {
+    filter_city_data () {
+      return this.postal_array.filter(name => name.miejscowosc.toLowerCase().includes(this.city.toLowerCase())).filter(name => name.wojewodztwo.includes('śląskie') || name.wojewodztwo.includes('pomorskie') || name.wojewodztwo.includes('dolnoslaskie') || name.wojewodztwo.includes('kujawsko-pomorskie') || name.wojewodztwo.includes('lubelskie') || name.wojewodztwo.includes('lubuskie') || name.wojewodztwo.includes('łódzkie') || name.wojewodztwo.includes('małopolskie') || name.wojewodztwo.includes('mazowieckie') || name.wojewodztwo.includes('opolskie') || name.wojewodztwo.includes('podkarpackie') || name.wojewodztwo.includes('podlaskie') || name.wojewodztwo.includes('świętokrzyskie') || name.wojewodztwo.includes('warmińsko-mazurskie') || name.wojewodztwo.includes('wielkopolskie') || name.wojewodztwo.includes('zachodnipomorskie'))
+    }
+  },
   name: 'Postal_code.vue',
   data () {
     return {
+      response_status: true,
       alert_text: '',
       array_data: [],
       city: '',
-      postal: ''
+      postal: '',
+      postal_array: []
     }
   },
   mounted () {
     const input = document.getElementById('postal_input')
+    const input2 = document.getElementById('postal_input2')
     input.addEventListener('keypress', keypress => {
+      if (keypress.key === 'Enter') {
+        this.onButtonClick()
+      }
+    })
+    input2.addEventListener('keypress', keypress => {
       if (keypress.key === 'Enter') {
         this.onButtonClick()
       }
@@ -60,8 +87,12 @@ export default {
       document.getElementById('alert').style.opacity = '0'
     },
     onButtonClick () {
+      if (!this.response_status) {
+        this.show_alert('Poczekaj na odpowiedź')
+        return
+      }
       if (this.city === '' && this.postal === '') {
-        this.show_alert('Wprowadź kod pocztowy !')
+        this.show_alert('Wprowadź kod pocztowy lub miasto!')
         return
       }
       if (this.city === '') {
@@ -83,6 +114,10 @@ export default {
           }
         }
       } else {
+        if (this.city.charAt(0) === 'ł') {
+          this.city = this.city.substring(1, this.city.length)
+          this.city = 'Ł' + this.city
+        }
         this.send_function(this.city, 'city')
       }
     },
@@ -97,6 +132,10 @@ export default {
       this.postal = ''
       document.getElementById('response').style.fontSize = '0'
       document.getElementById('response').style.opacity = '0'
+      document.getElementById('response').style.height = '0'
+    },
+    change_state_response () {
+      this.response_status = true
     },
     send_function (code, type) {
       const headers = {
@@ -106,6 +145,7 @@ export default {
         'x-rapidapi-key': 'c12d626061msh43653fb2a1f88cbp1d0a56jsndebedd586ca5'
       }
       this.array_data = []
+      this.postal_array = []
       if (type === 'postal') {
         axios
           .get('https://polish-zip-codes1.p.rapidapi.com/' + code, { headers })
@@ -125,10 +165,44 @@ export default {
           })
       }
       if (type === 'city') {
+        this.response_status = false
+        setTimeout(this.change_state_response, 20000)
+        this.postal_array = []
         axios
-          .get('http://kodpocztowy.intami.pl/city/' + code, { headers })
+          .get('https://polish-zip-codes1.p.rapidapi.com/city/' + code, { headers })
           .then(response => {
-            console.log(response.data)
+            const promises = []
+            for (const postalCodeElememt of response.data) {
+              promises.push(
+                axios
+                  .get('https://polish-zip-codes1.p.rapidapi.com/' + postalCodeElememt, { headers })
+                  .then(response => {
+                    let lastElement = null
+                    const dataArray = []
+                    for (const element of response.data) {
+                      if (lastElement !== element.miejscowosc) {
+                        dataArray.push(element)
+                      }
+                      lastElement = element.miejscowosc
+                    }
+                    for (const listElement of dataArray) {
+                      if (!listElement.nazwa) {
+                        if (!this.postal_array.find(elements => elements.miejscowosc === listElement.miejscowosc && elements.gmina === listElement.gmina && elements.wojewodztwo === listElement.wojewodztwo)) {
+                          this.postal_array.push(listElement)
+                        }
+                      }
+                    }
+                  })
+              )
+            }
+            Promise.all(promises).then(() => {
+              this.response_status = true
+            })
+          })
+          .catch(error => {
+            if (error.response.status === 404) {
+              this.show_alert('Nie znaleziono miasta')
+            }
           })
       }
       document.getElementById('response').style.fontSize = '15px'
@@ -181,6 +255,16 @@ export default {
   color: #98C1D9;
 }
 #response_card {
+  background-color: #293241;
+  border-radius: 2px;
+  padding: 5px 5px;
+  margin: 10px 5px;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-rows: auto;
+  grid-gap: 6px;
+}
+#response_card2 {
   background-color: #293241;
   border-radius: 2px;
   padding: 5px 5px;
