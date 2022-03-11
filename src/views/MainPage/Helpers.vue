7d38054f444196696db199fbccfec30d08640d33
Jakub Drzymała <ordenix@otavi.pl>
2022-03-11 12:08:43 +0100

    add pemitedlicence

--- src/views/MainPage/Helpers.vue
+++ src/views/MainPage/Helpers.vue
@@ -33,13 +33,9 @@
         <div class="icon"><img :src="require(`@/assets/others/logo_black.png`)" alt="Sign"/></div>
         <div class="label">Sklep mundurowy</div>
       </div>
-      <div class="element" @click="globalGoTo('/SPB/home/', 'SPB')">
+      <div v-if="false" class="element" @click="globalGoTo('/SPB/home/', 'SPB')">
         <div class="icon"><i class="fa-solid fa-gun"></i></div>
         <div class="label">ŚPB</div>
-      </div>
-      <div class="element" @click="globalGoTo('/UTO/home/', 'UTO')">
-        <div class="icon"><i class="fa-brands fa-accessible-icon"></i></div>
-        <div class="label">UTO</div>
       </div>
     </div>
   </div>
