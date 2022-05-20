2d372ec7b91e338bc7aebaac7f8e6cd436f541ca
Knapek14 <knapek14@gmail.com>
2022-05-20 18:14:10 +0200

    Poprawka kalkulatora nietrzeźwości
    Poprawka kalkulatora nietrzeźwości - dorzucenie informacji o konsekwencjach związanych z RD oraz przelicznik promile - mg

--- src/views/HelpersModule/DrunkCalc.vue
+++ src/views/HelpersModule/DrunkCalc.vue
@@ -3,14 +3,50 @@
     <div class="small_main_container">
       <div id="alert">{{alert_text}} <i class="fa-solid fa-triangle-exclamation"></i></div>
       <div class="master_title">Przelicznik miligramów na promile</div>
+        <div v-if="prom === 'undefined' || prom === ''" class="hidden">
       <div class="container">
         <div class="title">Wprowadź liczbę miligramów:</div>
         <input class="input" type="number" v-model="drunk" @change="on_change">
         <div class="title">mg/l</div>
       </div>
+      </div>
+       <div v-if="drunk === 'null' || drunk === '' " class="hidden">
+         <div class="container">
+        <div class="title">Wprowadź liczbę promili:</div>
+        <input class="input" type="number" v-model="prom" @change="on_changing">
+        <div class="title">‰</div>
+      </div>
+      </div>
       <br/>
       <div v-if="diff> 0">
         <div class="over_drunk">Wartość promili w organizmie wynosi {{du}} ‰</div>
+        <div v-if="du > '0.50'">
+        <hr style="border: 1px solid #EE6C4D;">
+        <div class="info">Jeśli badany to kierujący pojazdem mechanicznym - przestępstwo art. 178A KK - czynności procesowe</div>
+         </div>
+        <div v-else-if="du < '0.20'">
+        <hr style="border: 1px solid #EE6C4D;">
+            <div class="info">Brak znamion czynu zabronionego w rozumieniu przepisów UPRD</div>
+            </div>
+         <div v-else>
+         <hr style="border: 1px solid #EE6C4D;">
+         <div class="info">Jeśli badany to kierujący pojazdem mechanicznym - wykroczenie art. 87 § 1 KW - czynności 54 KPOW</div>
+        </div>
+      </div>
+      <div v-if="prm> 0">
+        <div class="over_drunk">Wartość miligramów w organizmie wynosi {{prms}} mg/l</div>
+         <div v-if="prms > '0.25'">
+        <hr style="border: 1px solid #EE6C4D;">
+        <div class="info">Jeśli badany kierujący pojazdem mechanicznym - przestępstwo art. 178A KK - czynności procesowe</div>
+         </div>
+          <div v-else-if="prms < '0.10'">
+         <hr style="border: 1px solid #EE6C4D;">
+          <div class="info">Brak znamion czynu zabronionego w rozumieniu przepisów UPRD</div>
+         </div>
+         <div v-else>
+         <hr style="border: 1px solid #EE6C4D;">
+         <div class="info">Jeśli badany to kierujący pojazdem mechanicznym - wykroczenie art. 87 § 1 KW - czynności 54 KPOW</div>
+         </div>
       </div>
   </div>
 </div>
@@ -29,8 +65,11 @@ export default {
     return {
       diff: 0,
       du: 0,
+      prm: 0,
+      prms: 0,
       promile: '2.1',
       drunk: '',
+      prom: '',
       alert_text: ''
     }
   },
@@ -38,7 +77,7 @@ export default {
     on_change () {
       this.diff = 0
       if (this.promile === '' || this.drunk === '') {
-        this.show_alert('Wprowadź miligramy')
+        this.show_alert('Wprowadź wartość')
       } else if (parseInt(this.drunk) > 10.62) {
         this.show_alert('Hmm... przeliczam ale... Rekordzista miał 10.62 mg/l')
         this.diff = this.drunk * this.promile
@@ -46,6 +85,19 @@ export default {
       } else {
         this.diff = this.drunk * this.promile
         this.du = this.diff.toFixed(2)
+      }
+    },
+    on_changing () {
+      this.prm = 0
+      if (this.promile === '' || this.prom === '') {
+        this.show_alert('Wprowadź wartość')
+      } else if (parseInt(this.prom) > 10.62) {
+        this.show_alert('Hmm... przeliczam ale... Rekordzista miał 22,3 ‰')
+        this.prm = this.prom / this.promile
+        this.prms = this.prm.toFixed(2)
+      } else {
+        this.prm = this.prom / this.promile
+        this.prms = this.prm.toFixed(2)
       }
     },
     destroy_Alert () {
@@ -105,6 +157,19 @@ hr{
   font-family: $text-family;
   padding: 10px;
 }
+.info {
+  color: #e0fbfc;
+  font-family: $text-family;
+  padding: 10px;
+  font-size: 12px;
+}
+.littleinfo {
+  color: #e0fbfc;
+  font-family: $text-family;
+  padding: 10px;
+  font-size: 10px;
+}
+
 @media only screen and (min-width: 700px) {
   .container {
     grid-template-columns: [first] 250px [line2] 150px [line3] 10px;
